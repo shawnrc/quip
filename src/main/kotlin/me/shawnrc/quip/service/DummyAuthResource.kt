37a145b8c2eba9df494b42287201899cfa5f0957
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-03-29 16:26:16 -0400

    something easier for jackson to serialize

--- src/main/kotlin/me/shawnrc/quip/service/DummyAuthResource.kt
+++ src/main/kotlin/me/shawnrc/quip/service/DummyAuthResource.kt
@@ -6,12 +6,13 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.MultivaluedMap
 
 @Path("/v1/balls")
 @Produces(MediaType.APPLICATION_JSON)
 class DummyAuthResource {
   @GET
-  fun echo(@Context headers: HttpHeaders): Map<*,*> {
+  fun echo(@Context headers: HttpHeaders): Map<String, List<String>> {
     return headers.requestHeaders
   }
 }
