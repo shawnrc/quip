4907687623c1fec78b0fa2469d628b52e9a22e06
Shawn Chowdhury <shawn.rc0@gmail.com>
2018-03-29 16:05:53 -0400

    adds header dump

--- src/main/kotlin/me/shawnrc/quip/service/DummyAuthResource.kt
+++ src/main/kotlin/me/shawnrc/quip/service/DummyAuthResource.kt
@@ -3,11 +3,15 @@ package me.shawnrc.quip.service
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.MediaType
 
 @Path("/v1/balls")
 @Produces(MediaType.APPLICATION_JSON)
 class DummyAuthResource {
   @GET
-  fun echo() = "Hello World"
+  fun echo(@Context headers: HttpHeaders): Map<*,*> {
+    return headers.requestHeaders
+  }
 }
